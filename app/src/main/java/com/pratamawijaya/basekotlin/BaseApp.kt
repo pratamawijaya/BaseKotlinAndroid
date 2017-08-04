116fbf99c85d0776491e6ac57188b8e5565da2fe
pratamawijaya <set.mnemonix@gmail.com>
2017-08-04 11:10:39 +0700

    add dependency

---
+++ app/src/main/java/com/pratamawijaya/basekotlin/BaseApp.kt
@@ -0,0 +1,13 @@
+package com.pratamawijaya.basekotlin
+
+import android.app.Application
+
+/**
+ * Created by pratama on 8/4/17.
+ */
+class BaseApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
