a647bbb76074cce03f1e02eda8d548f10a14b1e5
pratamawijaya <set.mnemonix@gmail.com>
2017-10-21 12:37:08 +0700

    refactor base fragment

--- app/src/main/java/com/pratamawijaya/basekotlin/presentation/base/BaseInjectedFragment.kt
+++ app/src/main/java/com/pratamawijaya/basekotlin/presentation/base/BaseInjectedFragment.kt
@@ -1,12 +1,6 @@
 package com.pratamawijaya.basekotlin.presentation.base
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import butterknife.ButterKnife
-import butterknife.Unbinder
 import com.pratamawijaya.basekotlin.BaseApp
 import com.pratamawijaya.basekotlin.di.component.ActivityComponent
 import com.pratamawijaya.basekotlin.di.module.ActivityModule
@@ -14,9 +8,7 @@ import com.pratamawijaya.basekotlin.di.module.ActivityModule
 /**
  * Created by pratama on 8/4/17.
  */
-abstract class BaseInjectedFragment : Fragment() {
-
-    private lateinit var unbinder: Unbinder
+abstract class BaseInjectedFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,22 +20,6 @@ abstract class BaseInjectedFragment : Fragment() {
                 .build()
 
         injectModule(activityComponent)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(getLayout(), container, false)
-    }
-
-    abstract fun getLayout(): Int
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        unbinder = ButterKnife.bind(this, view)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        unbinder.unbind()
     }
 
     abstract fun injectModule(activityComponent: ActivityComponent)
