905c4fe1f9024cb1ebb8ce65aea7029406a277fb
pratamawijaya <set.mnemonix@gmail.com>
2017-10-26 08:16:23 +0700

    upgrade build.gradle to android 3.0 final add linking debug tree with linenumber and class name

--- app/src/main/java/com/pratamawijaya/basekotlin/BaseApp.kt
+++ app/src/main/java/com/pratamawijaya/basekotlin/BaseApp.kt
@@ -8,6 +8,7 @@ import com.pratamawijaya.basekotlin.di.component.DaggerAppComponent
 import com.pratamawijaya.basekotlin.di.module.AppModule
 import com.pratamawijaya.basekotlin.di.module.DataModule
 import timber.log.Timber
+import java.util.regex.Pattern
 
 /**
  * Created by pratama on 8/4/17.
@@ -29,7 +30,7 @@ class BaseApp : Application() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(LinkingDebugTree())
         } else {
             Timber.plant(CrashlyticsTree())
         }
@@ -46,6 +47,38 @@ class BaseApp : Application() {
 //                Crashlytics.logException(throwable)
             }
         }
+    }
 
+    class LinkingDebugTree : Timber.Tree() {
+        companion object {
+            val CALL_STACK_INDEX = 4
+            val ANONYMOUS_CLASS: Pattern = Pattern.compile("(\\\\\$\\\\d+)+\$")
+        }
+
+        override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
+            val stackTrace = Throwable().stackTrace
+
+            if (stackTrace.size <= CALL_STACK_INDEX) {
+                throw IllegalStateException("Synthetic stacktrace didn't have enough elements: are you using proguard?")
+            }
+
+            val clazz = extractClassName(stackTrace[CALL_STACK_INDEX])
+            val linenumber = stackTrace[CALL_STACK_INDEX].lineNumber
+            val outMessage = ".($clazz.java:$linenumber) - $message"
+            super.log(priority, tag, outMessage, t)
+        }
+
+        /**
+         * Extract the class name without any anonymous class suffixes (e.g., {@code Foo$1}
+         * becomes {@code Foo}).
+         */
+        private fun extractClassName(stackTraceElement: StackTraceElement): String {
+            var tag = stackTraceElement.className
+            val matcher = ANONYMOUS_CLASS.matcher(tag)
+            if (matcher.find()) {
+                tag = matcher.replaceAll("")
+            }
+            return tag.substring(tag.lastIndexOf('.') + 1)
+        }
     }
 }
