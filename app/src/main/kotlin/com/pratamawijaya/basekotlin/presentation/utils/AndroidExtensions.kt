ba79afef09f1e1cbb025a00ca7fea1c60e5a5915
pratamawijaya <set.mnemonix@gmail.com>
2018-03-24 01:51:48 +0700

    remove glide

--- app/src/main/kotlin/com/pratamawijaya/basekotlin/presentation/utils/AndroidExtensions.kt
+++ app/src/main/kotlin/com/pratamawijaya/basekotlin/presentation/utils/AndroidExtensions.kt
@@ -3,7 +3,6 @@ package com.pratamawijaya.basekotlin.presentation.utils
 import android.app.Activity
 import android.app.Fragment
 import android.view.View
-import android.widget.ImageView
 import android.widget.Toast
 import com.pratamawijaya.basekotlin.BuildConfig
 
@@ -20,14 +19,6 @@ fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
 
 fun Fragment.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
     if (BuildConfig.DEBUG) Toast.makeText(this.activity, message, duration).show()
-}
-
-fun ImageView.loadimg(url:String){
-    GlideApp.with(this.context)
-            .load(url)
-            .placeholder(0)
-            .into(this)
-
 }
 
 /**
--- app/src/main/kotlin/com/pratamawijaya/basekotlin/presentation/utils/MyAppGlideModule.kt
+++ app/src/main/kotlin/com/pratamawijaya/basekotlin/presentation/utils/MyAppGlideModule.kt
@@ -1,10 +1,6 @@
 package com.pratamawijaya.basekotlin.presentation.utils
 
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
 /**
  * Created by pratama on 08/03/18.
  */
-@GlideModule
-class MyAppGlideModule : AppGlideModule()
+class MyAppGlideModule
