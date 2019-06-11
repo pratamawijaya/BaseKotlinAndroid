f703aaf86964b6c786a946904d22c450559493c5
Pratama Nur Wijaya <set.mnemonix@gmail.com>
2019-06-11 14:38:24 +0700

    initial setup base

---
+++ app/src/main/java/com/pratamawijaya/basekotlin/screens/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.pratamawijaya.basekotlin.screens.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pratamawijaya.basekotlin.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/pratamawijaya/basekotlin/screens/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.pratamawijaya.basekotlin.screens.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pratamawijaya.basekotlin.R
+import com.pratamawijaya.basekotlin.screens.home.HomeActivity
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        startActivity<HomeActivity>()
+    }
+}
