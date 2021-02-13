b535414bb45ab73d3c71c37d72be5196209fa188
pratamawijaya <set.mnemonix@gmail.com>
2021-02-13 08:06:15 +0700

    fix scoped component

--- app/src/main/java/com/pratama/baseandroid/di/module/ActivityModule.kt
+++ app/src/main/java/com/pratama/baseandroid/di/module/ActivityModule.kt
@@ -1,7 +1,6 @@
 package com.pratama.baseandroid.di.module
 
 import android.content.Context
-import com.pratama.baseandroid.BuildConfig
 import com.pratama.baseandroid.coreandroid.network.NetworkChecker
 import com.pratama.baseandroid.coreandroid.network.NetworkCheckerImpl
 import com.pratama.baseandroid.data.datasource.local.NewsLocalDatasource
@@ -9,7 +8,6 @@ import com.pratama.baseandroid.data.datasource.local.NewsLocalDatasourceImpl
 import com.pratama.baseandroid.data.datasource.local.db.AppDatabase
 import com.pratama.baseandroid.data.datasource.remote.NewsRemoteDatasource
 import com.pratama.baseandroid.data.datasource.remote.NewsRemoteDatasourceImpl
-import com.pratama.baseandroid.data.datasource.remote.interceptor.HeaderInterceptor
 import com.pratama.baseandroid.data.datasource.remote.service.NewsApiServices
 import com.pratama.baseandroid.data.repository.NewsRepository
 import com.pratama.baseandroid.data.repository.NewsRepositoryImpl
@@ -19,48 +17,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(ActivityComponent::class)
 class ActivityModule {
-
-    @Provides
-    @ActivityScoped
-    fun provideOkHttpClient(headerInterceptor: HeaderInterceptor) = if (BuildConfig.DEBUG) {
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-        OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .addInterceptor(headerInterceptor)
-            .build()
-    } else {
-        OkHttpClient.Builder()
-            .addInterceptor(headerInterceptor)
-            .build()
-    }
-
-    @Provides
-    @ActivityScoped
-    fun provideHeaderInterceptor(): HeaderInterceptor {
-        return HeaderInterceptor()
-    }
-
-    @Provides
-    @ActivityScoped
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-        .baseUrl("https://newsapi.org/v2/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(okHttpClient)
-        .build()
-
-    @Provides
-    @ActivityScoped
-    fun provideNewsApiServices(retrofit: Retrofit): NewsApiServices =
-        retrofit.create(NewsApiServices::class.java)
 
     @Provides
     @ActivityScoped
--- app/src/main/java/com/pratama/baseandroid/di/module/ApplicationModule.kt
+++ app/src/main/java/com/pratama/baseandroid/di/module/ApplicationModule.kt
@@ -2,12 +2,19 @@ package com.pratama.baseandroid.di.module
 
 import android.content.Context
 import androidx.room.Room
+import com.pratama.baseandroid.BuildConfig
 import com.pratama.baseandroid.data.datasource.local.db.AppDatabase
+import com.pratama.baseandroid.data.datasource.remote.interceptor.HeaderInterceptor
+import com.pratama.baseandroid.data.datasource.remote.service.NewsApiServices
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -22,5 +29,39 @@ class ApplicationModule {
             AppDatabase::class.java, "my_app_database"
         ).build()
     }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(headerInterceptor: HeaderInterceptor) = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(headerInterceptor)
+            .build()
+    } else {
+        OkHttpClient.Builder()
+            .addInterceptor(headerInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHeaderInterceptor(): HeaderInterceptor {
+        return HeaderInterceptor()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl("https://newsapi.org/v2/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideNewsApiServices(retrofit: Retrofit): NewsApiServices =
+        retrofit.create(NewsApiServices::class.java)
 
 }
