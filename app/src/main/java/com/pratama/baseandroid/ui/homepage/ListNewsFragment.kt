0a79b85ca424727d9b4e57de37f48311e60bfa9d
pratamawijaya <set.mnemonix@gmail.com>
2023-02-28 12:31:11 +0700

    upgrade dependency version

--- app/src/main/java/com/pratama/baseandroid/ui/homepage/ListNewsFragment.kt
+++ app/src/main/java/com/pratama/baseandroid/ui/homepage/ListNewsFragment.kt
@@ -56,6 +56,9 @@ class ListNewsFragment : BaseFragmentBinding<FragmentListNewsBinding>(), NewsIte
                         listNewsAdapter.add(NewsItem(it, this@ListNewsFragment))
                     }
                 }
+                else ->{
+                    
+                }
             }
         })
     }
