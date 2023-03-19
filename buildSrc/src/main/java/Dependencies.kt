de0080dc8fabadc44c69f3eaf3cb795630b8c152
pratamawijaya <set.mnemonix@gmail.com>
2023-03-19 10:48:24 +0700

    build doctor

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,6 @@
 object AndroidConfig {
     const val compileSdkVersion = 30
-    const val buildToolsVersion = "30.0.3"
+//    const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 21
     const val targetSdkVersion = 29
 }
